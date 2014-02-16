3885b528bd7aa50e1f8c2490116b0a41babfdb09
user <user@ubuntu.(none)>
2014-02-16 04:10:06 -0500

    fixed bugs & added words

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 var bgcolor = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#95a5a6','#f39c12'];
 
-var words = ["Indiana Jones","Spider Man","Sherlock Holmes","Albert Einstein","Winnie the Pooh","Shakespeare","Snoopy","Christopher Columbus","Abraham Lincoln","Isaac Newton","Scooby Doo","Voldemort", "Elvis Presley", "Dr. Seuss", "Darth Vader", "Yoda", "Mario", "The Hulk", "Santa Claus", "E.T", "The Lion King", "The Wizard of Oz", "The Lord of the Rings", "Batman", "Star Wars", "Shrek", "The Matrix", "The Terminator", "Rocky", "Willy Wonka and the Chocolate Factory", "The Simpsons", "American Dad", "Man vs. Wild", "Hell's Kitchen", "Everybody Hates Chris", "Wheel of Fortune", "SpongeBob SquarePants", "South Park", "Naruto", "Family Guy", "The Office", "Pokemon", "The Hobbit by J. R. R. Tolkien", "The Lion the Witch and the Wardrobe - C. S. Lewis", "Alice's Adventures In Wonderland by Lewis Carroll", "Twilight by Stephenie Meyer", "The Cat in the Hat by Dr. Seuss", "Hunger Games", "Thrift Shop by Macklemore", "Radioactive by Imagine Dragons", "Started from the Bottom by Drake", "Michael Jackson", "Justin Bieber", "Bill Gates"];
+var words = ["Indiana Jones","Spider Man","Sherlock Holmes","Albert Einstein","Winnie the Pooh","Shakespeare",
+			"Snoopy","Christopher Columbus","Abraham Lincoln","Isaac Newton","Scooby Doo","Voldemort", "Elvis Presley", 
+			"Dr. Seuss", "Darth Vader", "Yoda", "Mario", "The Hulk", "Santa Claus", "E.T", "The Lion King", "The Wizard of Oz", 
+			"The Lord of the Rings", "Batman", "Star Wars", "Shrek", "The Matrix", "The Terminator", "Rocky", 
+			"Willy Wonka and the Chocolate Factory", "The Simpsons", "American Dad", "Man vs. Wild", "Hell's Kitchen", 
+			"Everybody Hates Chris", "Wheel of Fortune", "SpongeBob SquarePants", "South Park", "Naruto", "Family Guy", 
+			"The Office", "Pokemon", "The Hobbit by J. R. R. Tolkien", "The Lion the Witch and the Wardrobe - C. S. Lewis", 
+			"Alice's Adventures In Wonderland by Lewis Carroll", "Twilight by Stephenie Meyer", "The Cat in the Hat by Dr. Seuss",
+			"Hunger Games", "Thrift Shop by Macklemore", "Radioactive by Imagine Dragons", "Started from the Bottom by Drake", 
+			"Michael Jackson", "Justin Bieber", "Bill Gates", 'Twerk', 'Fetch', 'Tweet', 'Swag', 'Bananas', 'Selfie', 'YOLO', 
+			'Snapchat', 'League of Legends', 'Phablet', 'Global Warming', 'Doge', 'Gangnam Style','Baby','Door','Banana','Finger','fence',
+			'Big','Swimming Pool', 'Sun', 'Church','Yo-yo', 'Boy', 'Bag', 'Alligator', 'Mouse', 'Birthday', 'Winter', 'Beach', 'Tree',
+			'Teacher', 'King', 'Telephone','Eye', 'Water', 'Jelly','Balloon', 'Toothbrush', 'Pants',
+			'Mom', 'Body', 'Bike','Toilet', 'Paper', 'Baseball', 'Pig','Lawn', 'Mower', 'Fire', 'School','Belt','Ice Cream', 'Drums',
+			'Spider', 'Shark', 'Seashell','Computer', 'Grandma', 'Pillow','Kite', 'Homework','Bed', 'Bird', 'Gum','Book', 'Dress', 'Queen',
+			'Puppy', 'Happy', 'Doctor',];
 
- 
+
 
 function randBG(){
 	var i = Math.floor( Math.random() * 8);
@@ -16,7 +31,7 @@ function changeBG(e){
 }
 
 function randWord(){
-	var i = Math.floor(Math.random() * 54);
+	var i = Math.floor(Math.random() * 125);
 	return words[i];
 }
 
