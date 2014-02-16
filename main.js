ca1cb7347b888e0fe1e19108dedcce033c38fc55
user <user@ubuntu.(none)>
2014-02-15 19:03:54 -0500

    hi

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 var bgcolor = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#f1c40f', '#e67e22', '#95a5a6','#f39c12'];
 
+var words = ["hello", "world", "bye"];
+
 function randBG(){
 	var i = Math.floor( Math.random() * 8);
 
-	//document.body.style.background = bgcolor[i];
+	return bgcolor[i];
 }
 
+function changeBG(e){
+	$(e).css('background', randBG());
+
+}
